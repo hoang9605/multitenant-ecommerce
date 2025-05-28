be555cec5a59707bda7754483e2c131acd685333
hoang9605 <haihoang962005@gmail.com>
2025-05-28 08:18:33 +0700

    05-search-done

---
+++ src/app/(app)/(home)/search-filter/subcategory-menu.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+import { Category } from "@/payload-types/";
+
+interface Props {
+    category: Category; 
+    isOpen: boolean;
+    position: { top: number; left: number };
+}
+
+export const SubcategoryMenu = ({ 
+    category,
+    isOpen, 
+    position 
+}: Props) => {
+    if (!isOpen || !category.subcategories || category.subcategories.length === 0) {
+        return null;
+    }
+
+    const backgroundColor = category.color || "white";
+
+    return (
+        <div
+            className="fixed z-100"
+            style={{
+                top: position.top,
+                left: position.left,
+            }}
+        >
+            {/* Create invisible bridge maintain hover */}
+            <div className="h-3 w-60" />
+            <div 
+            style={{ backgroundColor }}
+            className="w-60 text-black rounded-md overflow-hidden border">
+                <div>
+                    {category.subcategories?.map((subcategory: Category) => (
+                        <Link 
+                            key={subcategory.slug} 
+                            href="/" 
+                            className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center font-medium"
+                            >
+                            {subcategory.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
