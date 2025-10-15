f6085d12dfcb72a4e7a52adf3f6487c152475271
hoang9605 <haihoang962005@gmail.com>
2025-10-16 02:42:06 +0700

    19 checkout page

---
+++ src/modules/checkout/ui/components/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+import { generateTenantURL } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+    slug: string;
+}
+
+export const Navbar = ({ slug }: Props) => {
+    return (
+        <nav className="h-20 border-b border-black font-medium bg-white">
+            <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
+                <p className="text-xl">Checkout</p>
+                <Button 
+                    variant="outline"
+                    asChild
+                >
+                    <Link href={generateTenantURL(slug)}>
+                        Continue Shopping
+                    </Link>
+                </Button>
+            </div>
+        </nav>
+    );
+};
+
